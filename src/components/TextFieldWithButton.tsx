c0b986204ded466475132e5812dc597be87c56fd
Chenx Dust <chenx_dust@outlook.com>
2025-06-19 06:52:22 +0800

    feat: better TextFieldWithButton

--- src/components/TextFieldWithButton.tsx
+++ src/components/TextFieldWithButton.tsx
@@ -1,4 +1,4 @@
-import { ButtonItem, TextField } from "@decky/ui";
+import { DialogButton, Field, Focusable, TextField } from "@decky/ui";
 import { ChangeEventHandler, FC, ReactNode } from "react";
 
 export interface TextFieldWithButtonProps {
@@ -9,77 +9,56 @@ export interface TextFieldWithButtonProps {
   children?: ReactNode;
   value?: string;
   onChange?: ChangeEventHandler<HTMLInputElement>;
-  onClick?: (e: MouseEvent) => void;
+  onClick?: () => void;
   loading?: boolean;
   debugLabel?: string;
 }
 
 export const TextFieldWithButton: FC<TextFieldWithButtonProps> =
   (props: TextFieldWithButtonProps) => {
-    return (<>
-      <style>
-        {`
-          .tfwb-small-button > div {
-            padding-top: 0px !important;
-            padding-bottom: 0px !important;
-            background: none !important;
-            color: unset !important;
-          }
-          .tfwb-small-button button {
-            min-width: unset !important;
-          }
-          .tfwb-component {
-            padding-bottom: 10px;
-            padding-top: 10px;
-            position: relative;
-          }
-          .tfwb-component::after {
-            bottom: -0.5px;
-            height: 1px;
-            background: rgba(255,255,255,.1);
-            content: "";
-            position: absolute;
-            left: 0;
-            right: 0;
-          }
-        `}
-      </style>
-      <div class="tfwb-component">
-        {props.label && (
-          <div class="DialogLabel">
-            {props.label}
-          </div>
-        )}
-        <div style={{
-          display: "flex",
-          flexDirection: "row",
-          gap: "10px"
-        }}>
-          <div style={{ flexGrow: 1 }}>
-            <TextField
-              value={props.value}
-              disabled={props.disabled}
-              /* @ts-expect-error */
-              placeholder={props.placeholder}
-              onChange={props.onChange}
-            />
-          </div>
-          <div class="tfwb-small-button">
-            <ButtonItem
-              layout="below"
-              bottomSeparator="none"
+    return (
+      <Field
+        description={props.description}
+        label={
+          /* @ts-expect-error */
+          <Focusable style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            flexGrow: 1,
+            columnGap : '10px',
+          }}>
+            {props.label &&
+              <div class="DialogLabel" style={{ width: '100%' }}>
+                {props.label}
+              </div>
+            }
+            <div style={{ flexGrow: 1 }}>
+              <TextField
+                value={props.value}
+                disabled={props.disabled}
+                /* @ts-expect-error */
+                placeholder={props.placeholder}
+                onChange={props.onChange}
+              />
+            </div>
+            <DialogButton
+              style={{
+                height: '40px',
+                width: '40px',
+                padding: '10px 12px',
+                minWidth: '40px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
               disabled={props.disabled}
               onClick={props.onClick}
+              onOKButton={props.onClick}
             >
               {props.children}
-            </ButtonItem>
-          </div>
-          {props.description && (
-            <div class="DialogLabelExplainer">
-              {props.description}
-            </div>
-          )}
-        </div>
-      </div>
-    </>)
+            </DialogButton>
+          </Focusable>
+        }
+      />
+    )
   }
